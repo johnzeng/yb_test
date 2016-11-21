c2a16f20499e606881a74f67a7163775b1c0455e
zengda <zengda@yunba.io>
2016-11-21 17:28:28 +0800

    test get alias

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require("./config.js")
 
 var mqtt = require('mqtt')
 
-console.log(config)
-
 var client  = mqtt.connect(config.url, {
         protocolId:'MQIsdp',
         protocolVersion:3,
@@ -13,17 +11,13 @@ var client  = mqtt.connect(config.url, {
         password: config.password
         })
  
-console.log("now connect")
 
 client.on('connect', function () {
-  console.log("connect success")
   // test the basic sub and publish
   client.subscribe('normal_pub_sub', null , 
           function(error, granted) {
               if(null != error){
-                  console.log("sub error")
               }else{
-                  console.log("on suback is accept")
                   client.publish('normal_pub_sub', 'hello')
               }
           })
@@ -32,8 +26,7 @@ client.on('connect', function () {
   client.publish(',yali', 'test_alias_b', {qos : 1}, 
           function(error){
               if (null == error){
-                  console.log("set alias go back");
-                  client.publish(',yta/test_alias_b', 'hello')
+                  client.publish(',yta/test_alias_b', 'hello');
               }
           })
 
@@ -44,6 +37,10 @@ client.on('message', function (topic, message) {
   console.log(topic , message.toString());
   counter ++;
   if (counter === 2){
+      client.publish(',yaliget',"", {qos:1});
+
+  }
+  if (counter === 3){
     client.end();
   }
 })

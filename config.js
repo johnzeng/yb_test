efa24bbe37081017951be07dc99e033d56a0c252
zengda <zengda@yunba.io>
2016-11-24 16:26:01 +0800

    update config sample

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ global.config= {
     url : 'mqtt:example-mqtt-url',
     clientId:'example-clientId',
     username:'example-username',
-    password:'example-pwd'
+    password:'example-pwd',
+    token:'example-token'
 }
 
